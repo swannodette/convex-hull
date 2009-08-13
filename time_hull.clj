503250c09d68c4c5f0c9c6b4c33dc759f104a241
David Nolen <david.nolen@gmail.com>
2009-08-13 04:18:03 -0400

    * time_hull.clj: minor edit.

--- time_hull.clj
+++ time_hull.clj
@@ -1,7 +1,9 @@
 (ns time-hull
   (:use convex-hull))
 
-;; 2s to 2.5s
+(set! *warn-on-reflection* 1)
+
+;; 2s - 2.5s
 (def my-points (points 400000 rand-point))
 (let [hull-points (time (hull my-points))]
   (printf "Points: %d\n" (count hull-points))
